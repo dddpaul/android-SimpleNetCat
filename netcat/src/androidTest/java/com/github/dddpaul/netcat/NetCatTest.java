03f317d26a5239ea3d1c1b608c6c89bb0a20233b
Pavel Derendyaev <paul@reic.ru>
2014-07-05 09:35:28 +0400

    Join tests in one class

--- netcat/src/androidTest/java/com/github/dddpaul/netcat/NetCatTest.java
+++ netcat/src/androidTest/java/com/github/dddpaul/netcat/NetCatTest.java
@@ -1,8 +1,18 @@
+/**
+ * Require nc binary (netcat-openbsd package for Debian/Ubuntu).
+ */
 package com.github.dddpaul.netcat;
 
 import android.util.Log;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowLog;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -15,22 +25,91 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-import static com.github.dddpaul.netcat.NetCater.Op.*;
+import static com.github.dddpaul.netcat.NetCater.Op.CONNECT;
+import static com.github.dddpaul.netcat.NetCater.Op.DISCONNECT;
+import static com.github.dddpaul.netcat.NetCater.Op.LISTEN;
+import static com.github.dddpaul.netcat.NetCater.Op.RECEIVE;
+import static com.github.dddpaul.netcat.NetCater.Op.SEND;
 import static org.hamcrest.core.Is.is;
 
-public abstract class NetCatTester extends Assert implements NetCatListener
+@Config( emulateSdk = 18 )
+@RunWith( RobolectricTestRunner.class )
+public class NetCatTest extends Assert implements NetCatListener
 {
     final static String INPUT_TEST = "Input from this test";
     final static String INPUT_NC = "Input from netcat process";
     final static String HOST = "localhost";
     final String CLASS_NAME = ( (Object) this ).getClass().getSimpleName();
 
-    static List<String> nc = new ArrayList<>();
-
     NetCater netCat;
     NetCater.Result result;
     CountDownLatch latch;
     Process process;
+
+    @BeforeClass
+    public static void init()
+    {
+        ShadowLog.stream = System.out;
+    }
+
+    @Before
+    public void setUp() throws Exception
+    {
+        netCat = new NetCat( this );
+    }
+
+    @Test
+    public void testConnect() throws InterruptedException, IOException
+    {
+        String port = "9998";
+
+        // Start external nc listener
+        List<String> nc = new ArrayList<>();
+        nc.add( "nc" );
+        nc.add( "-v" );
+        nc.add( "-l" );
+        nc.add( port );
+        process = new ProcessBuilder( nc ).redirectErrorStream( true ).start();
+
+        // Execute connect operation after some delay
+        Thread.sleep( 500 );
+        Socket socket = connect( port );
+        netCat.setSocket( socket );
+
+        testNetCatOperations();
+    }
+
+    @Test
+    public void testListen() throws InterruptedException, IOException
+    {
+        String port = "9999";
+
+        // Connect to NetCat by external nc after some delay required for NetCat listener to start
+        final List<String> nc = new ArrayList<>();
+        nc.add( "nc" );
+        nc.add( "-v" );
+        nc.add( HOST );
+        nc.add( port );
+        new Thread( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try {
+                    Thread.sleep( 500 );
+                    process = new ProcessBuilder( nc ).redirectErrorStream( true ).start();
+                } catch( Exception e ) {
+                    e.printStackTrace();
+                }
+            }
+        } ).start();
+
+        // Start NetCat listener
+        Socket socket = listen( port );
+        netCat.setSocket( socket );
+
+        testNetCatOperations();
+    }
 
     @Override
     public void netCatIsStarted()
