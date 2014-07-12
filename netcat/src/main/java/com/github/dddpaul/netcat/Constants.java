4b37d9a1f657b792072ffdfb21c8485b226693d8
Pavel Derendyaev <paul@reic.ru>
2014-07-12 15:23:02 +0400

    Store connectTo strings is shared preferences and use it for autocomplete

---
+++ netcat/src/main/java/com/github/dddpaul/netcat/Constants.java
@@ -0,0 +1,7 @@
+package com.github.dddpaul.netcat;
+
+public class Constants
+{
+    public static final String SHARED_PREFS_NAME = "SimpleNetCat-Area";
+    public static final String CONNECT_TO_SET_KEY = "ConnectToSet";
+}
--- netcat/src/main/java/com/github/dddpaul/netcat/ui/MainFragment.java
+++ netcat/src/main/java/com/github/dddpaul/netcat/ui/MainFragment.java
@@ -1,5 +1,7 @@
 package com.github.dddpaul.netcat.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -7,11 +9,19 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.github.dddpaul.netcat.Constants;
 import com.github.dddpaul.netcat.R;
 import com.github.dddpaul.netcat.Utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -23,7 +33,7 @@ import static com.github.dddpaul.netcat.NetCater.Op.*;
 public class MainFragment extends Fragment
 {
     @InjectView( R.id.et_connect_to )
-    protected EditText connectToText;
+    protected AutoCompleteTextView connectToText;
 
     @InjectView( R.id.b_connect )
     protected Button connectButton;
@@ -34,6 +44,10 @@ public class MainFragment extends Fragment
     @InjectView( R.id.b_listen )
     protected Button listenButton;
 
+    private SharedPreferences prefs;
+    private Set<String> connectToSet;
+    private ArrayAdapter<String> connectToArrayAdapter;
+
     public static MainFragment newInstance()
     {
         return new MainFragment();
@@ -43,6 +57,10 @@ public class MainFragment extends Fragment
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
+        prefs = getActivity().getSharedPreferences( Constants.SHARED_PREFS_NAME, Context.MODE_PRIVATE );
+        connectToSet = prefs.getStringSet( Constants.CONNECT_TO_SET_KEY, new HashSet<String>() );
+        connectToArrayAdapter = new ArrayAdapter<>( getActivity(),
+                android.R.layout.simple_dropdown_item_1line, new ArrayList<>( connectToSet ));
     }
 
     @Override
@@ -57,13 +75,23 @@ public class MainFragment extends Fragment
                 updateUIWithValidation();
             }
         };
+        connectToText.setAdapter( connectToArrayAdapter );
         connectToText.addTextChangedListener( watcher );
         connectButton.setOnClickListener( new View.OnClickListener()
         {
             @Override
             public void onClick( View v )
             {
-                EventBus.getDefault().post( new FragmentEvent( CONNECT, connectToText.getText().toString() ) );
+                String connectTo = connectToText.getText().toString();
+                if( connectToSet.add( connectTo )) {
+                    connectToArrayAdapter.add( connectTo );
+                }
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putStringSet( Constants.CONNECT_TO_SET_KEY, connectToSet );
+                editor.apply();
+
+                EventBus.getDefault().post( new FragmentEvent( CONNECT, connectTo ) );
             }
         } );
         listenOnText.addTextChangedListener( watcher );
